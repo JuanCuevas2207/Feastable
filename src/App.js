8bb21002bdd115e954932689a53ad4c8bcc87555
JuanCuevas2207 <67801108+JuanCuevas2207@users.noreply.github.com>
2021-03-07 14:46:59 -0500

    Se reorganizan las carpetas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './Home/Home.css';
-import './Shared/Shared.css';
+import './components/navigationBar/NavigationBar.css';
+import './components/categoryBar/CategoryBar.css';
+import './components/title/Title.css';
 import React, {Component} from 'react';
-import NavigationBar from "./Shared/NavigationBar";
-import CategoryBar from "./Home/CategoryBar";
-import Title from "./Home/Title";
-import Content from "./Home/Content";
+import NavigationBar from "./components/navigationBar/NavigationBar";
+import CategoryBar from "./components/categoryBar/CategoryBar";
+import Title from "./components/title/Title";
 
 var App = () => 
 	<div> 
 		<NavigationBar /> 
 		<CategoryBar /> 
 		<Title text={'E N S A L A D A'}/>
-		<Content />
 	</div>
 
 export default App;
