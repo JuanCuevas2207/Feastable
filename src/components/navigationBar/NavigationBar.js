7088a928ff1270fdb375f401178d043fba76210f
JuanCuevas2207 <67801108+JuanCuevas2207@users.noreply.github.com>
2021-03-22 15:51:04 -0500

    Se corrigen librerias

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import NavBarStyles from './NavigationBar.module.css';
-import cx from 'classnames';
 import 'C:/Users/Juan Ma/Documents/Trabajos U Sabana/10mo semestre/Desarrollo con React JS/Feastable/node_modules/bootstrap/dist/css/bootstrap.css';
 
 const NavigationBar = () =>{
@@ -19,7 +18,7 @@ const NavigationBar = () =>{
   }
 
   return(
-    <div className={cx(NavBarStyles.header, 'py-2 align-items-center')}>  
+    <div className={[NavBarStyles.header, 'py-2 align-items-center'].join(" ")}>  
 
       <div className={'px-md-5 px-sm-4 px-3'}>
         <img src="assets/logo.png" height="35px" alt="Logo" className ={'float-left'}></img> 
@@ -31,18 +30,18 @@ const NavigationBar = () =>{
             placeholder = "Buscar una receta..."
             value = {input}
             name = "text"
-            className = {cx(NavBarStyles.buscador, 'col-9')}
+            className = {[NavBarStyles.buscador, 'col-9'].join(" ")}
             onChange = {inputText}
           />           
-          <button className={cx(NavBarStyles.button, 'col-3')} onClick={search}>Buscar</button>
+          <button className={[NavBarStyles.button, 'col-3'].join(" ")} onClick={search}>Buscar</button>
         </form>
 
-      <div className={cx('px-md-5 px-sm-3')}>
+      <div className={'px-md-5 px-sm-3'}>
         <div className={'float-right'}>
-          <a><img className={cx('mx-md-2 mx-1', NavBarStyles.menuImg)} src="assets/add.png" alt="Add icon" height="33px"></img></a>
-          <a><img className={cx('mx-md-2 mx-1', NavBarStyles.menuImg)} src="assets/save.png" alt="Save icon" height="30px"></img></a>
-          <a><img className={cx('mx-md-2 mx-1', NavBarStyles.menuImg)} src="assets/shopping-cart.png" alt="Shopping icon" height="30px"></img></a>
-          <a><img className={cx('mx-md-2 mx-1 rounded-circle', NavBarStyles.menuImg)} src="assets/profile-pic.jpg" alt="Picture icon" height="30px" width="30px"></img></a>
+          <a><img className={['mx-md-2 mx-1', NavBarStyles.menuImg].join(" ")} src="assets/add.png" alt="Add icon" height="33px"></img></a>
+          <a><img className={['mx-md-2 mx-1', NavBarStyles.menuImg].join(" ")} src="assets/save.png" alt="Save icon" height="30px"></img></a>
+          <a><img className={['mx-md-2 mx-1', NavBarStyles.menuImg].join(" ")} src="assets/shopping-cart.png" alt="Shopping icon" height="30px"></img></a>
+          <a><img className={['mx-md-2 mx-1 rounded-circle', NavBarStyles.menuImg].join(" ")} src="assets/profile-pic.jpg" alt="Picture icon" height="30px" width="30px"></img></a>
         </div>
       </div>   
     </div>
--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -1,10 +1,9 @@
 import recipeStyles from './Recipes.module.css';
-import cx from 'classnames';
 import 'C:/Users/Juan Ma/Documents/Trabajos U Sabana/10mo semestre/Desarrollo con React JS/Feastable/node_modules/bootstrap/dist/css/bootstrap.css';
 
 
 const Recipe = (props) =>(
-  <div className={cx(recipeStyles.recipe, 'py-3')}>
+  <div className={[recipeStyles.recipe, 'py-3'].join(" ")}>
     <h6 className={recipeStyles.time}>{props.time} min</h6>
     <h2 className={recipeStyles.recipeName}>{props.name}</h2>
     <h6 className={recipeStyles.rating}>{props.rating} stars</h6>
@@ -23,8 +22,10 @@ const recipesData = [
 const recipes = recipesData.map(recipeData => <Recipe {...recipeData} />);
 
 const Recipes = () => ( 
-  <div className={cx('row', 'justify-content-center', 'm-6')}>
-    {recipes}
+  <div className={"container"}>
+     <div className={'row justify-content-center m-6'}>
+      {recipes}
+    </div>
   </div>
 );
 
