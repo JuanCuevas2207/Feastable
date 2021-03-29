49e5f6138142289487be4db9a5f9119462bc3b09
Gabriel Nieves <gabrielnieves2000@gmail.com>
2021-03-29 15:44:19 -0500

    cambiando la ruta a los json

--- src/axiosComponents/axiosInstance.js
+++ src/axiosComponents/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL : "https://raw.githubusercontent.com/gabrielnievescosson/jsonFile/main" 
+    baseURL : "https://raw.githubusercontent.com/JuanCuevas2207/Feastable/gabrielnieves/dataFiles/" 
 });
 
 export default axiosInstance;
