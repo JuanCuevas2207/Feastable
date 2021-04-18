361dc8c32555f029b50d3dbef5ee01af450a9a60
JuanCuevas2207 <67801108+JuanCuevas2207@users.noreply.github.com>
2021-04-18 15:06:58 -0500

    Se modifica el estilo de las recetas

--- src/componentsHome/recipes/Recipe.js
+++ src/componentsHome/recipes/Recipe.js
@@ -1,11 +1,17 @@
 import recipeStyles from './Recipe.module.css';
 
 const Recipe = (props) =>(
-    <div className={[recipeStyles.recipe, 'py-3'].join(" ")} style={{backgroundImage: "url(assets/recipePics/"+props.image+")"}}>
-        <h6 className={recipeStyles.time}>{props.time} min</h6>
+    <div className={recipeStyles.recipe} style={{backgroundImage: "url(assets/recipePics/"+props.image+")"}}>      
+        <h6 className={recipeStyles.time}><img src="assets/icons/clock.png" height="20px" alt="Clock"></img> {props.time} min</h6>        
         <h2 className={recipeStyles.recipeName}>{props.name}</h2>
-        <h6 className={recipeStyles.rating}>{props.rating} stars</h6>
+        <h6 className={recipeStyles.rating}>
+        {props.rating < 1 ? <span className={recipeStyles.star}>☆</span> : <span className={recipeStyles.fullStar}>★</span>}
+        {props.rating < 2 ? <span className={recipeStyles.star}>☆</span> : <span className={recipeStyles.fullStar}>★</span>}
+        {props.rating < 3 ? <span className={recipeStyles.star}>☆</span> : <span className={recipeStyles.fullStar}>★</span>}
+        {props.rating < 4 ? <span className={recipeStyles.star}>☆</span> : <span className={recipeStyles.fullStar}>★</span>}
+        {props.rating < 5 ? <span className={recipeStyles.star}>☆</span> : <span className={recipeStyles.fullStar}>★</span>}
+        </h6>
     </div>
-  );
+);
 
-  export default Recipe;
+export default Recipe;
